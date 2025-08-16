11ceb26bcec18ac68cea25a5d026ddd9e84ef299
veer-deg-web <veerpratp.cse2022@ritroorkee.com>
2025-08-17 00:11:23 +0530

    why_choose_doctor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,13 @@ import DoctorsList from "../components/DoctorList";
 import QueryList from "../components/QueryList";
 import SearchBar from "../components/ui/SearchBar";
 import WhyChooseUs from "../components/WhyChooseUs.jsx";
+import WhyChooseDoctar from "../components/ui/why_choose_doctar.jsx";
+
 import { doctarFeatures } from "../data/featuredData.js";
 
 export default function Home() {
-  
   return (
-    <div className="min-h-screen bg-gray-100 relative">
+    <div className="min-h-screen bg-gray-100 relative px-2">
       {/* Purple Header Background - Compact */}
       <div className="bg-[#7551B2] w-full h-32 md:h-42 "></div>
       <div className="absolute left-1/2 transform -translate-x-1/2 top-8 -translate-y-1/2 w-full max-w-md px-4">
@@ -34,33 +35,48 @@ export default function Home() {
         <DoctorsList />
       </div>
 
-      <WhyChooseUs 
-        features={doctarFeatures}
-      />
+      <WhyChooseUs features={doctarFeatures} />
 
       <div className="flex flex-col items-center justify-center">
         <h2 className="text-center text-black text-xl">why Doctar?</h2>
-        <div className="flex items-center justify-between gap-4 p-4 text-sm text-black bg-[#F2F2F2] mt-16">
-          <div className="w-1/3 h-40">
-            <p className="text-xs">
+        <div className="flex   bg-grey  justify-between gap-2  text-sm text-black bg- mt-16 ">
+          <div className="relative   p-2 rounded-md flex-1 ">
+            <p className="text-black font-xs font-poppins">
               SIX offers a trusted, user-friendly platform tailored for used car
               dealers and individual seller.
             </p>
           </div>
-          <div className="w-1/3 h-40 border-l-4 border-[#5C1F9F] pl-4 ">
-            <p className="text-xs">
-              Connect Directly Owners option to Skip middlemen,negotiate freely,
-              and buy what you want within budge at your best price!
+          <div className="  left-0 top-4  h-8 w-1 bg-purple-600 rounded mr-2"></div>
+          <div className="relative  p-2 rounded-md flex-1 pl-[6px]">
+            <p className="text-black font-xs">
+              Connect Directly Owners option to Skip middlemen, negotiate
+              freely, and buy what you want within budget at your best price!
             </p>
           </div>
-          <div className="w-1/3 h-40 border-l-4 border-[#5C1F9F] pl-1 ">
-            <p className="text-xs">
+          <div className=" left-0 top-4 h-8 w-1 bg-purple-600 rounded mr-2"></div>
+          <div className="relative  p-2 rounded-md flex-1">
+            <p className="text-black font-xs">
               Connect with Real Buyers and Sellers Verified listings, genuine
               leads, and direct deals to ensure a faster & trusted experience.
             </p>
           </div>
         </div>
       </div>
+      <div>
+        <h2 className="">
+          {" "}
+          Why choose <span className="font-bold">DOCTAR</span>?
+        </h2>
+        <div>
+          <img
+            className=" mt-2 h-[477px] w-full rounded-xl"
+            src="docphoto.webp"
+            alt=""
+          />
+        </div>
+      </div>
+      <WhyChooseDoctar />
+      
     </div>
   );
 }
