423878675d35091c7f27e830bc0f706b434c762c
amarmurmu001 <iron57441@gmail.com>
2025-08-17 12:10:57 +0530

    improve: talkwithexpert comp

---
+++ src/components/TalkWithExpert.jsx
@@ -0,0 +1,56 @@
+function TalkWithExpert() {
+  return (
+    <div className=" p-2 flex flex-col  bg-[#f2f1f9] rounded-2xl mt-[32px] overflow-hidden">
+        <div className="w-full">
+          <img
+            src="/icons/expert.webp"
+            alt="Doctor consultation"
+            className="w-full h-auto rounded-t-2xl object-cover"
+          />
+        </div>
+
+        <div className="text-center gap-4 p-6 flex flex-col items-center justify-center">
+          <h2 className="text-[#7551b3] font-bold text-2xl mb-2">
+            Talk With Expert
+          </h2>
+
+          <p className="text-gray-700 text-base leading-relaxed mb-4 px-2">
+            Engage in genuine, confident discussions with experts, fostering
+            trust, sharing insights, and encouraging collaborative
+            problem-solving.
+          </p>
+
+          <button className="bg-black rounded-lg text-white py-3 px-6 text-lg font-medium mb-6 hover:bg-gray-800 transition-colors">
+            Ask a question
+          </button>
+
+          <h3 className="text-gray-800 text-lg font-medium mb-3">
+            Trending categories
+          </h3>
+
+          <div className="flex flex-wrap gap-6 text-sm">
+            <a href="#" className="text-[#7551b3] hover:underline">
+              New cars &gt;
+            </a>
+            <a href="#" className="text-[#7551b3] hover:underline">
+              New Bikes &gt;
+            </a>
+            <a href="#" className="text-[#7551b3] hover:underline">
+              property for sale &gt;
+            </a>
+            <a href="#" className="text-[#7551b3] hover:underline">
+              Used car &gt;
+            </a>
+            <a href="#" className="text-[#7551b3] hover:underline">
+              Used Bikes &gt;
+            </a>
+            <a href="#" className="text-[#7551b3] hover:underline">
+              property for rent &gt;
+            </a>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default TalkWithExpert
